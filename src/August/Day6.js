b617c21cc781d0b35958bdc8ef3fcea663bd8fc5
muh-younus <braq369@gmail.com>
2025-08-14 12:33:03 -0700

    find largest number

--- src/August/Day6.js
+++ src/August/Day6.js
@@ -1,56 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 function Palindrome1() {
+  function palindrome() {
+    let input = "Muhammad";
+    console.log(input);
+    let reverse = input.split("").reverse().join("");
+    console.log(reverse);
 
-
-    function palindrome(){
-
-        let input = "Muhammad";
-        console.log(input);
-        let reverse =input.split("").reverse().join("");
-        console.log(reverse);
-
-        if(input === reverse){
-            console.log("Palindrome");  
-        }
-        else{
-            console.log("not Palindrome")
-        }
-
+    if (input === reverse) {
+      console.log("Palindrome");
+    } else {
+      console.log("not Palindrome");
     }
+  }
 
-    function largestNumber(){
-
-        let array = [2,4,6,8,10,12,14,16];
-        let number = 0;
-        let count = 0;
-        for(let i =0; i < array.length; i++){
-            number = array[i];
-
-            if(number < array[i + 1]){
-
-
-                count = array[i + 1];
-                
-            }
-
-            
-        }
-        console.log("Largest Number is: " + count);
-    }
-
-    largestNumber(); 
-
-    useEffect(() => {   
-        //palindrome(); 
-             
-    },[]);  
-
-  return (
-    <div>
+  function largestNumber() {
+    debugger
+    let array = [ 4, 6, 40, 10, 12, 2];
+    let number = array[0];
+    
+    
+    for (let i = 0; i < array.length; i++) {
       
-    </div>
-  )
+
+      
+        if (array[i] > number) {
+          number = array[i];
+          console.log(number)
+        }
+      
+    }
+    console.log("Largest Number is: " + number);
+  }
+
+  largestNumber();
+
+  useEffect(() => {
+    //palindrome();
+  }, []);
+
+  return <div></div>;
 }
 
-export default Palindrome1
+export default Palindrome1;
